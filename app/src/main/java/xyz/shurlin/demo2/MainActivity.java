70ed75d1c0380cbfc685dee767bdb77e012ea838
zqf <3124475397@qq.com>
2025-11-01 21:47:51 +0800

    b1

--- app/src/main/java/xyz/shurlin/demo2/MainActivity.java
+++ app/src/main/java/xyz/shurlin/demo2/MainActivity.java
@@ -1,6 +1,8 @@
 package xyz.shurlin.demo2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // 处理 WindowInsets，将系统栏的 inset 设为 root view 的 padding（保留你的行为）
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
