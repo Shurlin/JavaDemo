0ea923f798b70047347c165b18bbe269061ab8de
Shurlin <1132141557@qq.com>
2025-11-20 00:51:58 +0800

    image show update

---
+++ app/src/main/java/xyz/shurlin/demo2/ui/list/ImageShowActivity.java
@@ -0,0 +1,21 @@
+package xyz.shurlin.demo2.ui.list;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import xyz.shurlin.demo2.R;
+
+public class ImageShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_image_show);
+    }
+}
--- app/src/main/java/xyz/shurlin/demo2/ui/main/HomeFragment.java
+++ app/src/main/java/xyz/shurlin/demo2/ui/main/HomeFragment.java
@@ -20,6 +20,8 @@ import xyz.shurlin.demo2.R;
 import xyz.shurlin.demo2.data.ToolMenuItem;
 import xyz.shurlin.demo2.ui.list.DigitalLogic.RCA_Activity;
 import xyz.shurlin.demo2.ui.list.EatShitActivity;
+import xyz.shurlin.demo2.ui.list.GuideActivity;
+import xyz.shurlin.demo2.ui.list.ImageShowActivity;
 import xyz.shurlin.demo2.ui.list.LouderActivity;
 import xyz.shurlin.demo2.ui.list.TestActivity;
 import xyz.shurlin.demo2.ui.list.wall.WallActivity;
@@ -62,12 +64,14 @@ public class HomeFragment extends Fragment {
     private void prepareMenu() {
         menuList.clear();
         // 各个栏目
+        menuList.add(new ToolMenuItem("item0", "使用指南", "初次使用请阅读指南", R.drawable.book1, GuideActivity.class));
         menuList.add(new ToolMenuItem("item1", "cdc", "进来帮cd来c", R.drawable.feedback, TestActivity.class));
         menuList.add(new ToolMenuItem("item2", "表白墙", "cd的表白墙", R.drawable.unlike, WallActivity.class));
 //        menuList.add(new ToolMenuItem("item3", "小吃街", "小吃街导航与评论", R.drawable.lollipop, SnackStreetActivity.class));
         menuList.add(new ToolMenuItem("item4", "看看谁更吵", "分贝测试器——klf不要再吵了", R.drawable.speak, LouderActivity.class));
 //        menuList.add(new ToolMenuItem("item5", "暗黑阅读器", "手指不放在屏幕上屏幕就会变黑", R.drawable.book1, DarkReaderActivity.class));
         menuList.add(new ToolMenuItem("item6", "DL", "Digital Logics", R.drawable.book1, RCA_Activity.class));
-        menuList.add(new ToolMenuItem("item6", "吃什么食", "434每日不知道吃什么", R.drawable.hotpot, EatShitActivity.class));
+        menuList.add(new ToolMenuItem("item7", "吃什么食", "434每日不知道吃什么", R.drawable.hotpot, EatShitActivity.class));
+        menuList.add(new ToolMenuItem("item8", "校卡展示", "存储校卡图片方便进出校门", R.drawable.file_cb, ImageShowActivity.class));
     }
 }
